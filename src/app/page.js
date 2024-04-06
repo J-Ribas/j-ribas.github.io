e3bb6b38f365b0ae92fd04f9f803b20cfdbb8aa7
J-Ribas <82511420+J-Ribas@users.noreply.github.com>
2024-04-06 02:10:54 +0000

    link

--- src/app/page.js
+++ src/app/page.js
@@ -12,7 +12,7 @@ function selectWallpaper() {
   ];
   return `bg-${names[Math.floor(Math.random() * names.length)]}`;
 }
-
+<a href="https://j-ribas.github.io/website" target="_blank">Visite o nosso site</a>
 export default function Home() {
   return (
     <main className={`overflow-hidden min-h-screen min-w-full grid place-items-center bg ${selectWallpaper()}`}>
